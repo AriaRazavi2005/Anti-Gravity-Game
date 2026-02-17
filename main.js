a7395f4fa24b536724d00f2ef33912b439fafa49
AriaRazavi2005 <ariarazavi105@gmail.com>
2026-02-17 23:08:50 +0330

    Update game with build and .gitignore

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        icon: path.join(__dirname, 'icon.png'), // Optional icon
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    });
+
+    win.loadFile('index.html');
+    win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
